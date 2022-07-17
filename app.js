ca9a63695db7a5511d94d792f743e35cf6bcde9c
Sihle Kambula <kambulasihle@gamil.com>
2022-07-17 21:23:19 +0200

    .

--- app.js
+++ app.js
@@ -1,42 +1,13 @@
 const express = require("express");
 const cors = require("cors");
-const axios = require("axios");
-const formData = require("form-data");
-const data = new formData();
+const AIroutes = require("./routes/ai");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
-
-app.get("/", (req, res) => {
-  res.json({ msg: "hello" });
-});
-
-// post request
-app.post("/", (req, res) => {
-  console.log("Request made");
-  const { url } = req.body;
-  data.append("image_url", url);
-
-  const config = {
-    method: "post",
-    url: "https://api.removal.ai/3.0/remove",
-    headers: {
-      "Rm-Token": "62cd59951a38b3.13811015",
-      ...data.getHeaders(),
-    },
-    data: data,
-  };
-  axios(config)
-    .then((response) => {
-      res.json(response.data);
-    })
-    .catch((error) => {
-      console.log(error);
-      res.json({ err: error });
-    });
-});
+app.use("/imageai", AIroutes);
 
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
