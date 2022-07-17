eecc3136cd35c5ff2c5794ce2878b5dbdcea05de
Sihle Kambula <kambulasihle@gamil.com>
2022-07-17 20:50:19 +0200

    testing

--- app.js
+++ app.js
@@ -5,9 +5,14 @@ const AIroutes = require("./routes/ai");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
-});
 app.use(express.json());
 app.use(cors());
 app.use("/imageai", AIroutes);
+
+app.get("/", (req, res) => {
+  res.status(200).send("Server up and running");
+});
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
