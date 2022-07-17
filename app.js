f7cb78add14ec81248bc198264f5d188d26f271d
Sihle Kambula <kambulasihle@gamil.com>
2022-07-17 21:19:09 +0200

    moved everything to main app

--- app.js
+++ app.js
@@ -1,16 +1,41 @@
 const express = require("express");
 const cors = require("cors");
-const AIroutes = require("./routes/ai");
-
+const axios = require("axios");
+const formData = require("form-data");
+const data = new formData();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
-app.use("/imageai", AIroutes);
 
 app.get("/", (req, res) => {
-  res.status(200).send("Server up and running");
+  res.json({ msg: "hello" });
+});
+
+// post request
+app.post("/", (req, res) => {
+  console.log("Request made");
+  const { url } = req.body;
+  data.append("image_url", url);
+
+  const config = {
+    method: "post",
+    url: "https://api.removal.ai/3.0/remove",
+    headers: {
+      "Rm-Token": "62cd59951a38b3.13811015",
+      ...data.getHeaders(),
+    },
+    data: data,
+  };
+  axios(config)
+    .then((response) => {
+      res.json(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.json({ err: error });
+    });
 });
 
 app.listen(PORT, () => {
